0b1f69af89e1fda03c0c1badfbbf259d572f0ae1
plasmarad <73721329+plasmarad@users.noreply.github.com>
2022-04-06 19:20:42 -0400

    Update Runtime.cpp

--- Source/Runtime.cpp
+++ Source/Runtime.cpp
@@ -1,8 +1,6 @@
-// RUNTIME.CPP 
+#include <Polaris.h>
 #include <Polaris/Window.h>
-#include <future
 
-int main(int argc, char **argv){
-    Polaris::Window Entry("Yottsu" 800, 600, false, true);
-    std::future<void> EventLoop = std::async(std::launch::async, &Polaris::Window::EventLoop, &Entry);
+Polaris::Instance Entry(){
+    
 }
